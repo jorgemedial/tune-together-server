93c8ceb33b2ef75c481cf67001505e7b24da2aa2
Jorge Medina <jorgemedial@gmail.com>
2024-05-05 00:36:21 +0200

    add user query endpoints

--- app.py
+++ app.py
@@ -1,53 +1,55 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+import pandas as pd
 
+from models.model import db, City, User, Style, SocialEvent, load_from_pandas
+from secret_keys import connection_string
 app = Flask(__name__)
 
-user_list = [
-    {
-        "user_id": 1,
-        "user_name": "uanalberto"
-    },
-    {
-        "user_id": 2,
-        "user_name": "marisabe"
-    },
-    {
-        "user_id": 3,
-        "user_name": "soyconchaentro"
-    },
-]
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+app.config["SQLALCHEMY_ECHO"] = True
+# initialize the app with the extension
+db.init_app(app)
 
-concert_list = [
-    {
-        "place_id": 1,
-        "place_name": "Rave en el Raval",
-        "place_description": "Cuidado no te roben",
-        "price": 20,
-        "estimated_max_hours": 3,
-        "match_ratio": 90
-    },
-    {
-        "place_id": 2,
-        "place_name": "Cumbia en el mar",
-        "place_description": "Dale mambo",
-        "price": 15,
-        "estimated_max_hours": 4,
-        "match_ratio": 70
-    }
-]
+with app.app_context():
+    db.create_all()
 
-# Contact API routes
-@app.route('/users', methods=['GET'])
-def users():
-    return jsonify({"message": "success", "users": user_list}), 200
+@app.route("/load_dummy_styles", methods=["GET"])
+def load_dummy_styles():
+    df_styles = pd.read_csv("data/outputs/styles.csv")
+    load_from_pandas(Style, df_styles, db.session)
+    return Response({"message": "success"}, status=200)
 
-@app.route('/search', methods=['POST'])
-def search():
-    return jsonify({"message": "success"}), 200 
+@app.route("/load_dummy_cities", methods=["GET"])
+def load_dummy_cities():
+    df_cities = pd.read_csv("data/outputs/cities.csv")
+    load_from_pandas(City, df_cities, db.session)
+    return Response({"message": "success"}, status=200)
 
-@app.route('/places', methods=['GET'])
-def places():
-    return jsonify({"message": "success", "concerts": concert_list}), 200
+@app.route("/load_dummy_users", methods=["GET"])
+def load_dummy_users():
+    df_users = pd.read_csv("data/outputs/users.csv")
+    load_from_pandas(User, df_users, db.session)
+    return Response({"message": "success"}, status=200)
+
+@app.route("/load_dummy_social_events", methods=["GET"])
+def load_dummy_social_events():
+    df = pd.read_csv("data/outputs/social_events.csv")
+    load_from_pandas(SocialEvent, df, db.session)
+    return Response({"message": "success"}, status=200)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    user_list = [user.to_dict() for user in User.query.all()]
+    return Response(user_list, status=200)
+
+@app.route("/user/<id>", methods=["GET"])
+def get_user(id):
+    user = User.query.filter_by(id=id).one()
+    return jsonify(user.to_dict()), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/model.py
@@ -0,0 +1,115 @@
+import datetime
+import json
+from typing import List
+from dataclasses import dataclass
+from sqlalchemy import ForeignKey, exc
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Base(DeclarativeBase):
+    pass
+            
+db = SQLAlchemy(model_class=Base)
+
+def load_from_pandas(cls, df, session):
+        try:
+            for index, row in df.iterrows():
+                base = cls(**{column_name: row[column_name] for column_name in df.columns})
+                session.add(base)
+            session.commit()
+        except exc.IntegrityError:
+            pass
+
+@dataclass
+class Style(db.Model):
+    __tablename__ = "style"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    users: Mapped[List["User"]] = relationship(back_populates="style")
+    social_events: Mapped[List["SocialEvent"]] = relationship(back_populates="style")
+
+    def add(id, name, session):
+        style = Style(id=id, name=name)
+        session.add(style)
+        session.commit()
+
+    
+
+
+@dataclass
+class StylesMatch(db.Model):
+    __tablename__ = "styles_match"
+
+    user_style_id: Mapped[str] = mapped_column(ForeignKey("style.id"), primary_key=True)
+    social_event_style_id: Mapped[str] = mapped_column(ForeignKey("style.id"), primary_key=True)
+    match_value: Mapped[float] = mapped_column(nullable=True)
+
+    def add(user_style, social_event_style, match_value, session):
+        styles_match = StylesMatch(user_style_id=user_style.id, social_event_style_id=social_event_style.id, match_value=match_value)
+        session.add(styles_match)
+        session.commit()
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    style_id: Mapped[str] = mapped_column(ForeignKey("style.id"), nullable=False)
+    style: Mapped["Style"] = relationship(back_populates="users")
+    city: Mapped["City"] = relationship(back_populates="users")
+    city_id: Mapped[str] = mapped_column(ForeignKey("city.id"))
+    departure_date: Mapped[datetime.date] = mapped_column(nullable=False)
+    arrival_date: Mapped[datetime.date] = mapped_column(nullable=False)
+
+    def to_dict(self):
+        return json.dumps({
+            "id": self.id,
+            "name": self.name,
+            "style": self.style.name,
+            "city": self.city.name,
+            "departure_time": self.departure_date.strftime("%Y/%m/%d"),
+            "arrival_date": self.arrival_date.strftime("%Y/%m/%d")
+        })
+
+
+@dataclass
+class City(db.Model):
+    __tablename__ = "city"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=True)
+    social_events: Mapped[List["SocialEvent"]] = relationship(back_populates="city")
+    users: Mapped[List["User"]] = relationship(back_populates="city")
+
+
+class SocialEvent(db.Model):
+    __tablename__ = "social_event"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    city_id: Mapped[str] = mapped_column(ForeignKey("city.id"), nullable=False)
+    city: Mapped["City"] = relationship(back_populates="social_events")
+    date: Mapped[datetime.date] = mapped_column(nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    style_id: Mapped[str] = mapped_column(ForeignKey("style.id"), nullable=False)
+    style: Mapped["Style"] = relationship(back_populates="social_events")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "city": self.city.name,
+            "date": self.date,
+            "price": self.price,
+            "style": self.style.name
+        }
+
+
+
